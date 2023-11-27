f5dd04eed1e735e1f0669b9fb678ae601fff0169
Marvin Haase <marvin.haase@soptim.de>
2023-11-27 17:58:40 +0100

    Added metadata files for different platforms

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ project.subprojects {
                 filter<ReplaceTokens>(
                         "tokens" to mapOf(
                                 "project.version" to project.version.toString(),
-                                "project.name" to project.name,
+                                "project.name" to (project.displayName),
                                 "project.group" to project.group.toString(),
                                 "project.id" to project.name,
                                 "project.description" to (project.description ?: "none")
