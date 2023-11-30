08b3f5d11b7a94d87aa1fadd93eeabc1b93bd650
Marfien <marvin.sempa.haase@gmail.com>
2023-11-30 19:05:38 +0100

    Added permissions for deployment --deploy

--- .github/workflows/scripts/gradle_build.sh
+++ .github/workflows/scripts/gradle_build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 GRADLE_COMMAND="clean"
 if [[ "$COMMIT_MESSAGE" == *"--deploy"* ]]; then
   # set version for snapshot deployment
@@ -14,4 +16,6 @@ if [[ $COMMIT_MESSAGE == *"--skip-test"* ]]; then
   GRADLE_COMMAND="$GRADLE_COMMAND -x check"
 fi
 
-./gradlew $GRADLE_COMMAND
+set -X
+
+./gradlew -Porg.gradle.deamon=false -Dpublish.user="$GITHUB_ACTOR" -Dpublish.password="$GITUB_TOKEN" "$GRADLE_COMMAND"
