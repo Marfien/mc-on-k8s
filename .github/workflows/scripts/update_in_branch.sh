58208bd95a6cd1d83f21469d135a9ce5c7a9dd25
Marfien <marvin.sempa.haase@gmail.com>
2023-12-01 23:26:35 +0100

    add debug to update_in_branch.sh --skip-ci

--- .github/workflows/scripts/update_in_branch.sh
+++ .github/workflows/scripts/update_in_branch.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
 read -ra branches <<< "$BRANCHES"
 
 for branch in ${branches[*]}; do
   echo "Updating version on [$branch] to $RELEASE_VERSION"
   git checkout -b "$branch" || true
-  sed -i s/^version=.*/version="$RELEASE_VERSION"/ gradle.properties
+  sed -i '' "s/^version=.*/version=${RELEASE_VERSION}/" gradle.properties || exit 1
   git add gradle.properties
   git commit -m "Updated version to $RELEASE_VERSION"
   git push origin "$branch"
