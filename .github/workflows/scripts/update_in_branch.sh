83d6823c1615673d7bbc7fcca2fa1c03f2cb7939
Marfien <marvin.sempa.haase@gmail.com>
2023-12-01 23:44:38 +0100

    removed extension declaration in sed --skip-ci

--- .github/workflows/scripts/update_in_branch.sh
+++ .github/workflows/scripts/update_in_branch.sh
@@ -8,7 +8,7 @@ read -ra branches <<< "$BRANCHES"
 for branch in ${branches[*]}; do
   echo "Updating version on [$branch] to $RELEASE_VERSION"
   git checkout -b "$branch" || true
-  sed -i '' "s/^version=.*/version=${RELEASE_VERSION}/" gradle.properties || exit 1
+  sed -i "s/^version=.*/version=${RELEASE_VERSION}/" gradle.properties || exit 1
   git add gradle.properties
   git commit -m "Updated version to $RELEASE_VERSION"
   git push origin "$branch"
