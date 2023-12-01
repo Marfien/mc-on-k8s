db7f95522fb60a8459ab55e3815cabe70753a383
Marfien <marvin.sempa.haase@gmail.com>
2023-12-01 23:06:40 +0100

    Added missing s to sed command --skip-ci

--- .github/workflows/scripts/update_in_branch.sh
+++ .github/workflows/scripts/update_in_branch.sh
@@ -5,7 +5,7 @@ read -ra branches <<< "$BRANCHES"
 for branch in ${branches[*]}; do
   echo "Updating version on [$branch] to $RELEASE_VERSION"
   git checkout -b "$branch" || true
-  sed -i "/^version=.*/version=$RELEASE_VERSION/" gradle.properties
+  sed -i "s/^version=.*/version=$RELEASE_VERSION/" gradle.properties
   git add gradle.properties
   git commit -m "Updated version to $RELEASE_VERSION"
   git push origin "$branch"
