cb0f2a8a9d45fac468ad25e8d134fcf6d222e73d
Marfien <marvin.sempa.haase@gmail.com>
2023-12-03 17:42:25 +0100

    release files & version detection --skip-ci

--- .github/workflows/scripts/release_version.sh
+++ .github/workflows/scripts/release_version.sh
@@ -12,10 +12,20 @@ increment_version() {
   echo "${array[*]}"
 }
 
+get_increment_index() {
+  if [[ "$INPUT_VERSION_UPGRADE" == "major" ]]; then return "0";
+  elif [[ "$INPUT_VERSION_UPGRADE" == "minor" ]]; then return "1";
+  elif [[ "$INPUT_VERSION_UPGRADE" == "patch" ]]; then return "2";
+  else
+    echo "Unknown increment: $INPUT_VERSION_UPGRADE";
+    exit 1;
+  fi
+}
+
 if [[ "$INPUT_CUSTOM_VERSION" ]]; then
   echo "RELEASE_VERSION=$INPUT_CUSTOM_VERSION" >> .env;
 else
   raw_version="$(grep "^version=" gradle.properties)"
-  version=$(increment_version "${raw_version#*=}" 1)
+  version=$(increment_version "${raw_version#*=}" "$(get_increment_index)")
   echo "RELEASE_VERSION=$version" >> .env
 fi
