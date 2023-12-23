8885e63713aa3a3b61a24959a6692e4fafde3d4f
Marfien <marvin.sempa.haase@gmail.com>
2023-12-23 17:39:48 +0100

    Loading docker images into kind cluster --skip-ci

--- deployment/startup.sh
+++ deployment/startup.sh
@@ -2,7 +2,11 @@
 
 kind create cluster --config kind.yaml
 
+# Load all images relating to this project into the kind cluster
+kind load docker-image $(docker images ghcr.io/marfien/mc-on-k8s/* | grep --color=NEVER -oE '^[^(REPOSIOTRY)][^ ]+')
+
 helm repo add agones-stable https://agones.dev/chats/stable
 helm install agones --namespace agones-system --create-namespace agones-stable/agones
 
+# Apply test deployments
 kubectl kustomize
