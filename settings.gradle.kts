6a64a2a56323b7f7fbbe7878937f5c1c393c1fdf
Marfien <marvin.sempa.haase@gmail.com>
2024-05-25 00:46:50 +0200

    Added operator project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,18 @@
+pluginManagement {
+    val quarkusPluginVersion: String by settings
+    val quarkusPluginId: String by settings
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+    }
+    plugins {
+        id(quarkusPluginId) version quarkusPluginVersion
+    }
+}
+
 rootProject.name = "minecraft-on-k8s"
 
-include("api")
 include("agones-model")
 include("model")
+include("operator")
