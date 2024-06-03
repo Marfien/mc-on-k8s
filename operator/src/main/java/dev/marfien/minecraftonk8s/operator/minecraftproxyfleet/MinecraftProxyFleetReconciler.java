2a38866f7391d5f78ae6c9022de289fcad123b97
Marfien <marvin.sempa.haase@gmail.com>
2024-06-03 21:33:47 +0200

    Moved methods around

--- operator/src/main/java/dev/marfien/minecraftonk8s/operator/minecraftproxyfleet/MinecraftProxyFleetReconciler.java
+++ operator/src/main/java/dev/marfien/minecraftonk8s/operator/minecraftproxyfleet/MinecraftProxyFleetReconciler.java
@@ -28,28 +28,6 @@ public class MinecraftProxyFleetReconciler implements Reconciler<MinecraftProxyF
     public static final String SELECTOR = "mconk8s.marfien.dev/managed-by-mcpf-reconciler";
 
     @Override
-    public DeleteControl cleanup(MinecraftProxyFleet resource,
-            Context<MinecraftProxyFleet> context) {
-        return DeleteControl.defaultDelete();
-    }
-
-    @Override
-    public ErrorStatusUpdateControl<MinecraftProxyFleet> updateErrorStatus(
-            MinecraftProxyFleet resource, Context<MinecraftProxyFleet> context, Exception e) {
-        return ErrorStatusUpdateControl.patchStatus(
-                resource.edit()
-                        .editStatus()
-                        .addNewCondition()
-                            .withStatus("False")
-                            .withType("Error")
-                            .withReason("ReconcileError")
-                            .withMessage(e.getMessage())
-                        .endCondition()
-                        .endStatus()
-                        .build());
-    }
-
-    @Override
     public UpdateControl<MinecraftProxyFleet> reconcile(MinecraftProxyFleet resource,
             Context<MinecraftProxyFleet> context) throws Exception {
         String clusterName = resource.getSpec().getClusterRef();
@@ -59,19 +37,40 @@ public class MinecraftProxyFleetReconciler implements Reconciler<MinecraftProxyF
         return UpdateControl.patchStatus(
                 resource.edit()
                         .editStatus()
-                            .withClusterId(cluster.getMetadata().getUid())
-                            .withReplicas(proxyFleet.getStatus().getReplicas())
-                            .withReadyReplicas(proxyFleet.getStatus().getReadyReplicas())
-                            .withAllocatedReplicas(proxyFleet.getStatus().getAllocatedReplicas())
-                            .addNewCondition()
-                                .withStatus("True")
-                                .withType("Reconciled")
-                                .withReason("FleetReconciled")
-                                .withMessage("The fleet has been reconciled successfully.")
-                            .endCondition()
+                        .withClusterId(cluster.getMetadata().getUid())
+                        .withReplicas(proxyFleet.getStatus().getReplicas())
+                        .withReadyReplicas(proxyFleet.getStatus().getReadyReplicas())
+                        .withAllocatedReplicas(proxyFleet.getStatus().getAllocatedReplicas())
+                        .addNewCondition()
+                        .withStatus("True")
+                        .withType("Reconciled")
+                        .withReason("FleetReconciled")
+                        .withMessage("The fleet has been reconciled successfully.")
+                        .endCondition()
                         .endStatus()
                         .build()
         );
+    }
 
+    @Override
+    public ErrorStatusUpdateControl<MinecraftProxyFleet> updateErrorStatus(
+            MinecraftProxyFleet resource, Context<MinecraftProxyFleet> context, Exception e) {
+        return ErrorStatusUpdateControl.patchStatus(
+                resource.edit()
+                        .editStatus()
+                            .addNewCondition()
+                                .withStatus("False")
+                                .withType("Error")
+                                .withReason("ReconcileError")
+                                .withMessage(e.getMessage())
+                            .endCondition()
+                        .endStatus()
+                        .build());
+    }
+
+    @Override
+    public DeleteControl cleanup(MinecraftProxyFleet resource,
+            Context<MinecraftProxyFleet> context) {
+        return DeleteControl.defaultDelete();
     }
 }
