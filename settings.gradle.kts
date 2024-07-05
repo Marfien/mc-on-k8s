648f044e06413ba230069cc38430ee2067458083
Marfien <marvin.sempa.haase@gmail.com>
2024-07-06 01:24:49 +0200

    Clean up settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,21 @@ pluginManagement {
 
 rootProject.name = "minecraft-on-k8s"
 
-include(":packages:kubernetes:agones-model")
-include(":packages:kubernetes:model")
-include(":packages:kubernetes:operator")
+include(
+        ":packages:kubernetes:agones-model",
+        ":packages:kubernetes:model",
+        ":packages:kubernetes:operator"
+)
 
-include(":packages:clients:proxy:proxy-api")
+include(
+        ":packages:clients:proxy:proxy-api",
+        ":packages:clients:proxy:proxy-impl-bungeecord",
+        ":packages:clients:proxy:proxy-impl-velocity"
+)
+
+include(
+        ":packages:clients:gameserver:gameserver-api",
+        ":packages:clients:gameserver:gameserver-impl-bukkit",
+        ":packages:clients:gameserver:gameserver-impl-sponge",
+        ":packages:clients:gameserver:gameserver-impl-minestom"
+)
