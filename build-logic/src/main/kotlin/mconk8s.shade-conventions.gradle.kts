9474f64b195d72fa07081156129b7b2a725cf9d6
Marfien <marvin.sempa.haase@gmail.com>
2024-07-10 23:19:01 +0200

    Adding todo

--- build-logic/src/main/kotlin/mconk8s.shade-conventions.gradle.kts
+++ build-logic/src/main/kotlin/mconk8s.shade-conventions.gradle.kts
@@ -1,5 +1,6 @@
 import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import gradle.kotlin.dsl.accessors._8c47cae829ea3d03260d5ff13fb2398e.build
 
 apply<ShadowPlugin>()
 
@@ -11,4 +12,8 @@ tasks.withType<ShadowJar> {
         include(project(":packages:kubernetes:model"))
         include(project(":packages:kubernetes:agones-model"))
     }
+}
+
+tasks.build {
+    dependsOn("shadowJar")
 }
