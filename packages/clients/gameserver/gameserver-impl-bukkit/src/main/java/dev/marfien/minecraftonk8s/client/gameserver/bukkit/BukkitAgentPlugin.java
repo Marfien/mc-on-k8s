57a77f320377400fbf6661febd7c565473768ccd
Marfien <contact@marfien.dev>
2025-02-08 02:00:52 +0100

    ref: removed legacy code

--- packages/clients/gameserver/gameserver-impl-bukkit/src/main/java/dev/marfien/minecraftonk8s/client/gameserver/bukkit/BukkitAgentPlugin.java
+++ packages/clients/gameserver/gameserver-impl-bukkit/src/main/java/dev/marfien/minecraftonk8s/client/gameserver/bukkit/BukkitAgentPlugin.java
@@ -6,7 +6,6 @@ import dev.marfien.minecraftonk8s.client.common.hook.PostPlayerConnectHook;
 import dev.marfien.minecraftonk8s.client.proxy.agent.GameServerAgent;
 import dev.marfien.minecraftonk8s.client.proxy.agent.GameServerInterface;
 import dev.marfien.minecraftonk8s.client.proxy.agent.configuration.GameServerEnvironmentConfiguration;
-import java.lang.reflect.Method;
 import java.util.function.Consumer;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
@@ -15,7 +14,6 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.event.player.PlayerLoginEvent.Result;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
@@ -24,22 +22,6 @@ public class BukkitAgentPlugin extends JavaPlugin implements GameServerInterface
 
     private final GameServerAgent<GameServerInterface, GameServerEnvironmentConfiguration> agent
             = new GameServerAgent<>(this, new GameServerEnvironmentConfiguration());
-
-    private static final Method ADVENTURE_DISALLOW_METHOD;
-
-    static {
-        Method method;
-        try {
-            Class<PlayerLoginEvent> clazz = PlayerLoginEvent.class;
-            method = clazz.getMethod("disallow", Result.class, Component.class);
-        } catch (NoSuchMethodException e) {
-            // Adventure text is not supported
-            // Then we will use legacy text instead
-            method = null;
-        }
-
-        ADVENTURE_DISALLOW_METHOD = method;
-    }
 
     @Override
     public void onEnable() {
